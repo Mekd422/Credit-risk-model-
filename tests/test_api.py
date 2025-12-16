5e8033cc5b11eb44b511893f3aa91285cebc52da
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-12-16 23:45:09 +0300

    updated

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,17 @@
 from fastapi.testclient import TestClient
 from src.api.main import app
 
+
 client = TestClient(app)
+
 
 def test_predict_endpoint():
     payload = {
         "feature_1": 123,
         "feature_2": 0.5,
-
     }
+
     response = client.post("/predict", json=payload)
+
     assert response.status_code == 200
     assert "risk_probability" in response.json()
